4e001781c75a38e7404d0d708ae97b0710d94abb
Tushar Samaniya <tusharsamaniya4956@gmail.com>
2026-03-06 23:10:26 +0530

    created the project for pinecode db

---
+++ src/main/java/com/ResearchMind/WorkingPineconeDbApplication.java
@@ -0,0 +1,13 @@
+package com.ResearchMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingPineconeDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingPineconeDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ResearchMind/WorkingPineconeDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ResearchMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingPineconeDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
